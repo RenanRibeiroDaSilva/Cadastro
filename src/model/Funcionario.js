0ba34862809b11bb497f7ec697f87008146c4bf6
Renan Ribeiro <renan_n64@hotmail.com>
2021-10-05 19:15:11 -0300

    Apresentação Aula

--- src/model/Funcionario.js
+++ src/model/Funcionario.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const FuncionarioSchema = mongoose.Schema({
     nome: {type: String},
-    idade: {type: String},
+    idade: {type: String}, // Definir number
     genero: {type: String},
     profissao: {type: String},
     status : {type: String, enum:['ativo', 'inativo'], default: 'ativo'}
